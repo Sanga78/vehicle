9cb56cd964b5519d94ddc0b7674fadab63b392d6
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-05-01 23:16:03 +0300

    Update new.js
    added car images

--- js/new.js
+++ js/new.js
@@ -8,10 +8,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // "Cars for Sale"
 const carsForSaleData = [
-    { src: "../images/car-1.jpg", model: "Mazda demio", price: "30,000", description: "Description for Car Model 1", colors: "Red, Blue, Black", seats: "4", images: ['../images/24_LEG_gallery_int_03.jpg', '../images/car-1.jpg', '../images/car-1.jpg']},
-    { src: "../images/car-2.jpg", model: "Prado", price: "20,000", description: "Description for Car Model 2", colors: "Red, Blue, Black", seats: "2", images: ['front-view-1.jpg', 'rear-view-1.jpg', 'inner-view-1.jpg']},
-    { src: "../images/car-3.jpg", model: "Probox", price: "50,000", description: "Description for Car Model 3", colors: "Red, Blue, Black", seats: "5", images: ['front-view-1.jpg', 'rear-view-1.jpg', 'inner-view-1.jpg']},
-    { src: "../images/car-4.jpg", model: "Harrier", price: "10,000", description: "Description for Car Model 4", colors: "Red, Blue, Black", seats: "1", images: ['front-view-1.jpg', 'rear-view-1.jpg', 'inner-view-1.jpg']},
+    { src: "../images/car-1.jpg", model: "Mazda demio", price: "30,000", description: "Description for Car Model 1", colors: "Red, Blue, Black", seats: "4", images: ['../images/24_LEG_gallery_int_03.jpg', '../images/JDPA_2021 Subaru Legacy Limited XT Gray Interior Dashboard.webp', '../images/Screenshot-2023-09-03-145752.png']},
+    { src: "../images/car-2.jpg", model: "Prado", price: "20,000", description: "Description for Car Model 2", colors: "Red, Blue, Black", seats: "2", images: ['../images/24_LEG_gallery_int_03.jpg', '../images/JDPA_2021 Subaru Legacy Limited XT Gray Interior Dashboard.webp', '../images/Screenshot-2023-09-03-145752.png']},
+    { src: "../images/car-3.jpg", model: "Probox", price: "50,000", description: "Description for Car Model 3", colors: "Red, Blue, Black", seats: "5", images: ['../images/24_LEG_gallery_int_03.jpg', '../images/JDPA_2021 Subaru Legacy Limited XT Gray Interior Dashboard.webp', '../images/Screenshot-2023-09-03-145752.png']},
+    { src: "../images/car-4.jpg", model: "Harrier", price: "10,000", description: "Description for Car Model 4", colors: "Red, Blue, Black", seats: "1", images: ['../images/24_LEG_gallery_int_03.jpg', '../images/JDPA_2021 Subaru Legacy Limited XT Gray Interior Dashboard.webp', '../images/Screenshot-2023-09-03-145752.png']},
 ];
 
 function loadCarsForSale() {
